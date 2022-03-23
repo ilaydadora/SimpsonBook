51130d1d2fda96bc75f0230152b555184a954142
ilaydadora <100840400+ilaydadora@users.noreply.github.com>
2022-03-23 15:25:20 +0300

    SimpsonBook
    SimpsonBook

---
+++ SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,27 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by İlayda Metin on 10.03.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+        
+    }
+    
+    
+    
+}
